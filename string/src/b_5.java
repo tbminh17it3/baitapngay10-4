e73d0ac61c90c24e580f6de6bd89c87bbb40bbe9
= <=>
2018-04-10 22:24:27 +0700

    first commit

---
+++ string/src/b_5.java
@@ -0,0 +1,12 @@
+public class b_5 {
+    
+   public static void main(String[] args)
+    {
+        String str = "xin chao cac ban.";
+
+        String upper_str = str.toUpperCase();
+
+        System.out.println("Chuoi ban dau : " + str);
+        System.out.println("Chuoi bang chu hoa : " + upper_str);
+    }
+}
